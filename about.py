304c78f95925ed2f8f0d82e81c93def5f0f89172
symp1ex <symplex@live.com>
2026-06-05 19:41:47 +0300

    fix about.py

--- about.py
+++ about.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 service_name = "POSRelayd"
-version = "2.5.3.1"
+version = "2.5.4.1"
 
 '''
 в зависимости от того, как запускается служба, нужно менять переменную current_path
